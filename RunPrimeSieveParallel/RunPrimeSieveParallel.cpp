ba2b463bda0a39087ca9b3c36b02f1d5b6827bb3
Frank Bodholdt Jakobsen <frabj@ece.au.dk>
2023-08-04 14:29:52 +0200

    Added C++ run program

--- RunPrimeSieveParallel/RunPrimeSieveParallel.cpp
+++ RunPrimeSieveParallel/RunPrimeSieveParallel.cpp
@@ -17,12 +17,13 @@ int main()
     logfile.close();
 
     unsigned long long memValues[] = {
+        16000000000,
         8000000000, 4000000000, 2000000000, 1000000000, 500000000, 250000000, 100000000,
         50000000, 20000000, 10000000, 8000000, 6000000, 4000000, 2000000, 1000000
     };
     int numMemValues = sizeof(memValues) / sizeof(memValues[0]);
 
-    int sieveValues[] = { 1, 2, 4, 5, 6, 8 };
+    int sieveValues[] = { 1, 2, 4, 5, 6, 8, 10, 12, 14, 16 };
     int numSieveValues = sizeof(sieveValues) / sizeof(sieveValues[0]);
 
     for (int s = 0; s < numSieveValues; s++)
@@ -31,10 +32,12 @@ int main()
         {
             std::ostringstream commandLine;
 
-            commandLine << "C:\\Users\\au237297\\source\\repos\\PrimeSieveParallel\\x64\\Release\\PrimeSieveParallel.exe ";
+            commandLine << "..\\..\\PrimeSieveParallel\\x64\\Release\\PrimeSieveParallel.exe ";
             commandLine << " -p " << maxPrime;
             commandLine << " -s " << sieveValues[s];
-            commandLine << " -m " << memValues[m];
+            commandLine << " -m " << memValues[m] << std::endl;
+
+            std::cout << commandLine.str();
 
             system(commandLine.str().c_str());
         }
